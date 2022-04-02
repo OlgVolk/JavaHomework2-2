6b5be4a3775d7a0ccc1662ca20a29369b0d3493a
Olga Volkova <olyaafl@mail.ru>
2022-04-02 15:28:13 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int startingScore = 100; // начальный счет
+        int replenishmentAmount = 2000; // сумма пополнения
+        int sumForBonus = 100; // сумма для начисления бонуса
+
+
+        int bonusRuble;
+        if (replenishmentAmount <= 1000) {
+            bonusRuble = replenishmentAmount;
+
+        } else {
+            bonusRuble = (replenishmentAmount / sumForBonus) + replenishmentAmount;
+
+        }
+        System.out.println("Вам начислено:" + bonusRuble);
+
+        int grandTotal = 2020;
+    }
+}
